2a3199222124dfdb20244c9a311be39b05ef74d3
empyrosx <empyrosx@yandex.ru>
2017-05-31 22:49:51 +0300

    Main activity logic.

--- app/src/main/java/com/github/empyrosx/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/github/empyrosx/flagquiz/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.github.empyrosx.flagquiz;
 
+import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,5 +19,17 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void updateGuessRows(SharedPreferences preferences) {
+
+    }
+
+    public void updateRegions(SharedPreferences preferences) {
+
+    }
+
+    public void resetQuiz() {
+
     }
 }
